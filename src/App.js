5fbdd5a893e28484cf450a043eaee8ac6e10fc6d
lubinets <lubineckarina@gmail.com>
2023-02-20 23:30:33 +0100

    Credentials added to the footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,34 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <header className="App-header"></header>
+        <footer className="App-footer text-center">
+          <a
+            href="https://github.com/lubinets/dictionary-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>{" "}
+          by{" "}
+          <a
+            href="https://www.linkedin.com/in/karina-lubinets-70309b11a/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Karina Lubinets
+          </a>
+          , hosted on{" "}
+          <a
+            href="https://melodious-paprenjak-bfc577.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
